0113ec228b6a4d391a5113e5dfb879802d6914a4
cppchriscpp <admin@cpprograms.net>
2021-03-05 00:30:07 -0500

    Upgrade python to fix syntax error

--- BasiliskII/src/Unix/_emenv.sh
+++ BasiliskII/src/Unix/_emenv.sh
@@ -4,9 +4,7 @@ if [[ -n "${macemujs_conf_wasm:-}" ]]; then
   use_wasm="y"
 fi
 emflgs=""
-# @cppchriscpp change
-# emflgs+=" -s TOTAL_MEMORY=536870912"
-emflgs+=" -s TOTAL_MEMORY=536896000"
+emflgs+=" -s TOTAL_MEMORY=536870912"
 emflgs+=" -s FORCE_FILESYSTEM=1"
 if [ -z $use_wasm ]; then
 emflgs+=" -s ASM_JS=1"
